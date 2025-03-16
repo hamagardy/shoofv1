3dc7958138e814e40d7ef5a66c4e2d1fc579539d
hamagardy <hgardy2@gmail.com>
2025-03-17 02:42:17 +0300

    all

---
+++ src/Info.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+function Info() {
+  return (
+    <div className="container">
+      <h1>Employee Info</h1>
+      <p>
+        This is a placeholder for employee information. Let me know what to add!
+      </p>
+    </div>
+  );
+}
+
+export default Info;
